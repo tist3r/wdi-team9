8c906c89d4ffbcfddea846b52b758efcd0db3880
Thomas Schmidt <schmidt.g.thomas@outlook.de>
2022-11-01 11:26:57 +0100

    handeled uncaught number format exception

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/model/CompanyXMLReader.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/model/CompanyXMLReader.java
@@ -1,9 +1,12 @@
 package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model;
 
+import java.lang.NumberFormatException;
+
 import org.w3c.dom.Node;
 
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.model.io.XMLMatchableReader;
+
 
 public class CompanyXMLReader extends XMLMatchableReader<Company, Attribute>{
 
@@ -18,26 +21,55 @@ public class CompanyXMLReader extends XMLMatchableReader<Company, Attribute>{
 		company.setName(getValueFromChildElement(node, "Name"));
 		company.setUrl(getValueFromChildElement(node, "Domain"));
         company.setIndustries(getListFromChildElement(node, "Industries"));
-        company.setYearFounded(Integer.parseInt(getValueFromChildElement(node, "Year_founded")));
+
+        try{
+            company.setYearFounded(Integer.parseInt(getValueFromChildElement(node, "Year_founded")));
+        } catch (NumberFormatException e){
+            company.setYearFounded(null);
+        }
+        
         company.setSizeRange(getValueFromChildElement(node, "Size_range"));
         company.setLocality(getValueFromChildElement(node, "Locality"));
         company.setCountry(getValueFromChildElement(node, "Country"));
         company.setLinkedinUrl(getValueFromChildElement(node, "Linkedin_url"));
-        company.setCurrEmpEst(Integer.parseInt(getValueFromChildElement(node, "Current_employee_est")));
-        company.setTotalEmpEst(Integer.parseInt(getValueFromChildElement(node, "Total_employee_est")));
+
+        try{
+            company.setCurrEmpEst(Integer.parseInt(getValueFromChildElement(node, "Current_employee_est")));
+        } catch (NumberFormatException e){
+            company.setCurrEmpEst(null);
+        }
+
+        try{
+            company.setTotalEmpEst(Integer.parseInt(getValueFromChildElement(node, "Total_employee_est")));
+        } catch (NumberFormatException e){
+            company.setTotalEmpEst(null);
+        }
         
         //TODO: Sales, Profit, etc.
 
 
         company.setSector(getValueFromChildElement(node, "Sector"));
         company.setGlobalRank(getValueFromChildElement(node, "Global_rank"));
-        company.setLatitude(Double.parseDouble(getValueFromChildElement(node, "Latitude")));
-        company.setLongitude(Double.parseDouble(getValueFromChildElement(node, "Longitude")));
+
+
+        try{
+            company.setLatitude(Double.parseDouble(getValueFromChildElement(node, "Latitude")));
+        } catch (NumberFormatException e){
+            company.setLatitude(Double.NaN);
+        }
+
+        try{
+            company.setLongitude(Double.parseDouble(getValueFromChildElement(node, "Longitude")));
+        } catch (NumberFormatException e){
+            company.setLongitude(Double.NaN);
+        }
+
+        
         company.setCeos(getListFromChildElement(node, "CEOs"));
 
 
 
-        return null;
+        return company;
     }
 
     
