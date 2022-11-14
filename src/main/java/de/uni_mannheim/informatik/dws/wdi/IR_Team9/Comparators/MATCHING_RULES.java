7c821c82505edf6d5931cb2ebcd1e5f22e52b6fb
Thomas Schmidt <schmidt.g.thomas@outlook.de>
2022-11-14 12:42:32 +0100

    changed mr9 to rm frequent tokens

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/IR_Team9/Comparators/MATCHING_RULES.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/IR_Team9/Comparators/MATCHING_RULES.java
@@ -10,7 +10,7 @@ import de.uni_mannheim.informatik.dws.winter.model.MatchingGoldStandard;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 
 public class MATCHING_RULES {
-    public static int NUM_MATCHING_RULES = 7;
+    public static int NUM_MATCHING_RULES = 8;
 
     public static String mr1Description;
     public static String mr2Description;
@@ -52,6 +52,7 @@ public class MATCHING_RULES {
             case 5: return getMR5(thresh, jaccardWeight);
             case 6: return getMR6(thresh);
             case 7: return getMR7(thresh, ds1, ds2, gsTrain);
+            case 8: return getMR8(thresh, ds1, ds2, gsTrain);
             default: throw new IndexOutOfBoundsException(String.format("Blocker with id %d does not exist, max is %d", id, NUM_MATCHING_RULES));
         }
     }
@@ -173,5 +174,38 @@ public class MATCHING_RULES {
 
         return rule;
     }
+
+
+    public static MatchingRule<Company, Attribute> getMR8(double thresh, HashedDataSet<Company, Attribute> ds1, HashedDataSet<Company, Attribute> ds2, MatchingGoldStandard gsTrain){
+        mr8Description = "Weka tree matching rule with postprocessing parameters";
+
+        // create a matching rule & provide classifier, options
+        String tree = "J48"; // new instance of tree
+        String options[] = new String[1];
+        options[0] = "-U";
+        WekaMatchingRule<Company, Attribute> rule = new WekaMatchingRule<>(thresh, tree, options);
+
+        // add comparators
+        rule.addComparator(new CompanyNameComparatorJaccardNgram(3, false, 0.55f));
+        rule.addComparator(new CompanyNameComparatorJaccardNgram(3, true, 0.55f));
+        rule.addComparator(new CompanyNameComparatorJaccardNgram(4, true, 0.55f));
+        rule.addComparator(new CompanyNameComparatorJaccardNgram(4, false, 0.55f));
+        rule.addComparator(new CompanyNameComparatorLevenshtein(true,0.55f));
+        rule.addComparator(new CompanyNameComparatorLevenshtein(false,0.55f));
+        rule.addComparator(new CompanyNameComparatorJaccardToken(0.4f, false));
+
+        // train the matching rule's model
+		RuleLearner<Company, Attribute> learner = new RuleLearner<>();
+		learner.learnMatchingRule(ds1, ds2, null, rule, gsTrain);
+
+		//logger.info(String.format("Matching rule is:\n%s", rule.getModelDescription()));
+
+        return rule;
+    }
+
+
+
+
+
     
 }
