e0876d3b83e72733923e84f4560d25d69d414098
Imanol Gonzalez <imanolgvallejo@gmail.com>
2022-11-22 13:43:17 +0100

    DataFusion package and class creation

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/IR_Team9/DataFusion/DataFusion_Main.java
@@ -0,0 +1,10 @@
+package de.uni_mannheim.informatik.dws.wdi.IR_Team9.DataFusion;
+
+public class DataFusion_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
