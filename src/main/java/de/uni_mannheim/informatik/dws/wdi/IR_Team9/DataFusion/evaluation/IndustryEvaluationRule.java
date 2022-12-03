ffe96d63041d53306f86851467e52c6385feefc3
Thomas Schmidt <schmidt.g.thomas@outlook.de>
2022-12-03 12:23:11 +0100

    changes evaluation rule

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/IR_Team9/DataFusion/evaluation/IndustryEvaluationRule.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/IR_Team9/DataFusion/evaluation/IndustryEvaluationRule.java
@@ -1,22 +1,34 @@
 package de.uni_mannheim.informatik.dws.wdi.IR_Team9.DataFusion.evaluation;
 
+import java.util.List;
+
 import de.uni_mannheim.informatik.dws.wdi.IR_Team9.model.Company;
 import de.uni_mannheim.informatik.dws.winter.datafusion.EvaluationRule;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.similarity.SimilarityMeasure;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
 import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccardSimilarity;
 
 public class IndustryEvaluationRule extends EvaluationRule <Company, Attribute> {
 
-	SimilarityMeasure<String> sim = new TokenizingJaccardSimilarity();
+	SimilarityMeasure<String> sim = new LevenshteinSimilarity();
 
 	@Override
 	public boolean isEqual(Company record1, Company record2, Attribute schemaElement) {
-		// the title is correct if all tokens are there, but the order does not
-		// matter
-		return sim.calculate(String.valueOf(record1.getIndustries()), String.valueOf(record2.getName())) == 1.0;
+		List<String> industries1 = record1.getIndustries();
+		List<String> industries2 = record2.getIndustries();
+
+		for(String i : industries1){
+			for(String j:industries2){
+				double similarity = sim.calculate(i,j);
+				if(similarity >= 0.9){
+					return true;
+				}
+		}}
+
+		return false;
 	}
 
 	@Override
